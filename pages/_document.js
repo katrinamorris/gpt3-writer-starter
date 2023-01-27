1e85809bcbeffff57ec9b5410a8e9051861c80e0
Katrina Morris <info@katrina-morris.com>
2023-01-27 01:49:02 -0500

    built out and styled for Violet Verse

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ export default function Document() {
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
+        <link rel="preload" href="https://www.violetverse.io/_next/static/css/d319ecee7268c79a.css" as="style"/>
+        <link rel="stylesheet" href="https://www.violetverse.io/_next/static/css/d319ecee7268c79a.css" data-n-g=""/>
+        <link rel="preload" href="https://www.violetverse.io/_next/static/css/8b173632210cad19.css" as="style"/>
+        <link rel="stylesheet" href="https://www.violetverse.io/_next/static/css/8b173632210cad19.css" data-n-p=""/>
+        <noscript data-n-css=""></noscript>
       </Head>
       <body>
         <Main />
